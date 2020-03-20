9d1e0b561d23bbe92f04b8a43811509ea802fc1e
ziord <ray25844@gmail.com>
2020-03-20 14:24:48 +0100

    updates

--- web2pdf/core/w2pconverter.py
+++ web2pdf/core/w2pconverter.py
@@ -4,15 +4,11 @@ from termcolor import colored
 import time, shutil
 import datetime
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
 from bs4 import BeautifulSoup
 import requests
 from PyPDF2 import (PdfFileReader, PdfFileWriter)
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import (Key, Controller)
 
 
 class PlatformNotSupportedException(Exception):
